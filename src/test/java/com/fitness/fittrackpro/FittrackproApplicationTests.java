367d648917dbf402765334dea5f81883f6c26010
JunaidJamshid123 <jamshidjunaid763@gmail.com>
2026-05-17 22:55:43 +0500

    Add Spring Boot project files

---
+++ src/test/java/com/fitness/fittrackpro/FittrackproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.fittrackpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
